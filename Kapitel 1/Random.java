6489a433d62bc9ef5131000a35a7ec1e7256410d
DoctorThomasElliot <doctorthomaselliot@googlemail.com>
2018-12-17 06:38:56 +0100

    Improved Math.random()

--- Kapitel 1/Random.java
+++ Kapitel 1/Random.java
@@ -6,6 +6,16 @@ public class Random {
 		x = Math.random();
 		System.out.println("Math.random x = " + x);
 	
+		System.out.println("");
+		System.out.println("Part 2");
+		int a = 0;
+		
+		while (a < 20) {
+			double y = Math.random()*10;
+			System.out.println("Math.random y * 10 = " + y);
+			a = a + 1;
+		}
+	
 	}
 
 }
