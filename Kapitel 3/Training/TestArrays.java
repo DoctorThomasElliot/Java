e5cb865fde5515f9b8b01857e245407ccd80f2bf
DoctorThomasElliot <doctorthomaselliot@googlemail.com>
2018-12-28 08:40:32 +0100

    Chapter 3 Training

---
+++ Kapitel 3/Training/TestArrays.java
@@ -0,0 +1,33 @@
+class TestArrays{
+
+	public static void main(String[] args){
+	
+		String[] inseln = new String[4];
+		int[] index = new int[4];
+		
+		int y = 0;
+		
+		inseln[0] = "Bermudas";
+		inseln[1] = "Fidschi";
+		inseln[2] = "Azoren";
+		inseln[3] = "Kuba";
+		
+		index[0] = 1;
+		index[1] = 3;
+		index[2] = 0;
+		index[3] = 2;
+		
+		int ref;
+		while (y < 4){
+			
+			ref = index[y];
+			
+			System.out.print("Insel = ");
+			System.out.println(inseln[ref]);
+			y = y + 1;
+			
+		}
+	
+	}
+
+}
